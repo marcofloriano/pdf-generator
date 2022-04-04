ef4860a086c15bb7176b961b8204c1748ee35fbb
marcofloriano <marcofloriano@gmail.com>
2022-04-04 11:10:29 -0300

    CSS experiments

--- index.php
+++ index.php
@@ -15,11 +15,32 @@ $pdf->SetFont('helvetica', '', 12, '', true);
 $pdf->AddPage();
  
 // date and invoice no
+// $pdf->Write(0, "\n", '', 0, 'C', true, 0, false, false, 0);
+$pdf->writeHTML("Setor9 - Negócios Online");
+// $pdf->writeHTML("<b>DATE:</b> 31/03/2022");
+$pdf->writeHTML("www.setor9.com.br");
+$pdf->writeHTML("Tel. 12-981924785");
 $pdf->Write(0, "\n", '', 0, 'C', true, 0, false, false, 0);
-$pdf->writeHTML("Setor9 - Negócios Online<br> 31/03/2022");
-$pdf->writeHTML("<b>DATE:</b> 31/03/2022");
-$pdf->writeHTML("<b>INVOICE#</b>12");
-$pdf->Write(0, "\n", '', 0, 'C', true, 0, false, false, 0);
+
+// Set some content to print
+$html = <<<EOD
+<h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
+<i>This is the first example of TCPDF library.</i>
+<p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
+<p>Please check the source code documentation and other examples for further information.</p>
+<p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>
+EOD;
+// Print text using writeHTMLCell()
+$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+
+//
+$html = <<<EOD
+<div style="font-size: 18px; background-color:#8297ac;color:white; padding:20px;">
+Fatura [N.º EQX034]
+</div>
+EOD;
+// Print text using writeHTMLCell()
+$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
  
 // address
 $pdf->writeHTML("84 Norton Street,");
